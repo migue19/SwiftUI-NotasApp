2a4732c4e044cc1619542af1d2f2a4972a1b0fdc
Miguel Mexicano Herrera <miguel.mexicano@hey.inc>
2024-05-27 11:39:24 -0600

    Initial Commit

---
+++ NotasApp/NotasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasAppApp.swift
+//  NotasApp
+//
+//  Created by MIGUEL MEXICANO HERRERA on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NotasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
